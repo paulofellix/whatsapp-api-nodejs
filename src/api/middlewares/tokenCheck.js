eb495c2b615b15e20a4f05e74e44ce3d0c1bc4bb
Paulo Félix <paulofellix@gmail.com>
2023-04-11 17:10:19 -0400

    fix: ignore token check some browser routes

--- src/api/middlewares/tokenCheck.js
+++ src/api/middlewares/tokenCheck.js
@@ -1,8 +1,16 @@
 const config = require('../../config/config')
 
+const ignorePaths = ['/status', '/qr']
+
 function tokenVerification(req, res, next) {
     const bearer = req.headers.authorization
     const token = bearer?.slice(7)?.toString()
+    const skip = ignorePaths.some((path) => req.path.includes(path))
+
+    if (skip) {
+        return next()
+    }
+
     if (!token) {
         return res.status(403).send({
             error: true,
